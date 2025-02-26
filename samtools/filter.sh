2637b22103c09110c28c58646c5192767a477bde
LinearParadox <alexei.martsinkovskiy@gmail.com>
2025-02-26 23:27:58 +0000

    modify filtering to remove unneeded sorted bams

--- samtools/filter.sh
+++ samtools/filter.sh
@@ -13,6 +13,8 @@ gcloud storage cp ${workdir}/outs/${sample}/bams/sorted.bam .
 gcloud storage cp ${workdir}/outs/${sample}/bams/sorted.bam.bai .
 chroms=($(cat filter_chroms.txt | cut -f1 | tr '\n' ' '))
 samtools view -@ $(($3/2)) -h sorted.bam | python3 filter_mt.py - - "${chroms[@]}" | samtools view -@ $(($3/2)) -bh -f 3 -o filtered.bam
+rm sorted.bam
+rm sorted.bam.bai
 samtools sort -@ $3 -m $4 -o filtered_sorted.bam filtered.bam
 samtools index -@ $3 filtered_sorted.bam
 gcloud storage cp filtered_sorted.b* ${workdir}/outs/${sample}/bams/
